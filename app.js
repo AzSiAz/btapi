d3a4a73fb79bb60216959e6586bd132745b6660c
AzSiAz <slider.perso@live.fr>
2017-10-08 17:42:47 +0200

    merge upstream

--- app.js
+++ app.js
@@ -9,14 +9,20 @@ const app = express()
 const port = process.env.PORT || 3003
 
 // express app setup
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'pug')
-app.use(compress())
-app.use(logger('dev'))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(cookieParser())
-app.use(express.static(path.join(__dirname, 'public')))
+app
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'pug')
+  .use(compress())
+  .use(logger('dev'))
+  .use(bodyParser.json())
+  .use(bodyParser.urlencoded({ extended: false }))
+  .use(cookieParser())
+  .use(express.static(path.join(__dirname, 'public')))
+  .use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    next();
+  })
 
 // Format JSON in browser
 app.set('json spaces', 2)
