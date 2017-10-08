60d12f67deb66b41d2e8d6d905046177bedec991
AzSiAz <slider.perso@live.fr>
2017-10-08 13:11:12 +0200

    update

--- app.js
+++ app.js
@@ -1,46 +1,45 @@
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var compress = require('compression');
+var express = require('express')
+var path = require('path')
+var logger = require('morgan')
+var cookieParser = require('cookie-parser')
+var bodyParser = require('body-parser')
+var compress = require('compression')
 
-var app = express();
+var app = express()
 
 // express app setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-app.use(compress());
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'jade')
+app.use(compress())
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
 // Format JSON in browser
-app.set('json spaces', 2);
-
+app.set('json spaces', 2)
 
 //routes to use for the application
-app.use('/', require('./routes/index'));
-app.use('/api', require('./routes/api'));
-
+app.use('/', require('./routes/index'))
+app.use('/api', require('./routes/api'))
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
+  var err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
+  res.status(err.status || 500)
+  res.render('error', {
+    message: err.message,
+    error: {}
+  })
+})
+
 const port = process.env.PORT || 3003
-app.listen(port, '0.0.0.0');
+app.listen(port, '0.0.0.0')
